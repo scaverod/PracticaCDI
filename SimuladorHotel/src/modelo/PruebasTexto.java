5aa2cfd91878eb37b9c625b5e92950efb91195dc
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2018-03-27 17:29:52 +0200

    Pruebas para el texto

---
+++ SimuladorHotel/src/modelo/PruebasTexto.java
@@ -0,0 +1,10 @@
+package modelo;
+
+public class PruebasTexto {
+
+	public static void main(String[] args) {
+		Texto t = new Texto(Texto.español);
+		t.getIdiomas();
+	}
+
+}
--- SimuladorHotel/src/modelo/Texto.java
+++ SimuladorHotel/src/modelo/Texto.java
@@ -1,5 +1,10 @@
 package modelo;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Texto {
 	public static String español = "ESP";
 	public static String english = "ENG";
@@ -10,5 +15,17 @@ public class Texto {
 	
 	public Texto(String idioma) {
 		this.idioma = idioma;
+	}
+	
+	public void getIdiomas() {
+		try {
+			Stream s = Files.list(Paths.get("Z:\\PracticaCDI\\SimuladorHotel\\src\\iconos"));
+			Object[] a = s.toArray();
+			for (int i = 0 ; i < a.length ; i++)
+				System.out.println(a[i].toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
