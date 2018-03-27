00573bc751fb3381a3fc0f18a79186b7dee6bd29
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2018-03-27 13:43:30 +0200

    Agregada pantalla de login
    OJO: el layout es temporal y debe cambiarse a CardLayout

--- SimuladorHotel/src/vista/Ventana.java
+++ SimuladorHotel/src/vista/Ventana.java
@@ -6,6 +6,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.LayoutStyle.ComponentPlacement;
@@ -17,11 +18,17 @@ import java.awt.Font;
 import java.awt.CardLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import javax.swing.JSeparator;
 
 public class Ventana extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private JPasswordField campoPassword;
+	private JPasswordField campoUsuario;
 
 	public Ventana() {
 		try {
@@ -51,6 +58,103 @@ public class Ventana extends JFrame {
 		label.setIcon(new ImageIcon(Ventana.class.getResource("/iconos/marcoTablet.png")));
 		label.setBounds(130, 3, 1080, 690);
 		layeredPane.add(label);
+		
+		JPanel pantallaLogin = new JPanel();
+		layeredPane.setLayer(pantallaLogin, 2);
+		pantallaLogin.setBounds(195, 51, 951, 594);
+		layeredPane.add(pantallaLogin);
+		
+		JLabel lblInicieSesinPara = new JLabel("Inicie sesi\u00F3n para usar la aplicaci\u00F3n");
+		lblInicieSesinPara.setHorizontalAlignment(SwingConstants.CENTER);
+		lblInicieSesinPara.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		
+		JPanel panelDatosSesion = new JPanel();
+		GroupLayout gl_pantallaLogin = new GroupLayout(pantallaLogin);
+		gl_pantallaLogin.setHorizontalGroup(
+			gl_pantallaLogin.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_pantallaLogin.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_pantallaLogin.createParallelGroup(Alignment.TRAILING)
+						.addComponent(panelDatosSesion, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 931, Short.MAX_VALUE)
+						.addComponent(lblInicieSesinPara, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 931, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		gl_pantallaLogin.setVerticalGroup(
+			gl_pantallaLogin.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_pantallaLogin.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblInicieSesinPara, GroupLayout.DEFAULT_SIZE, 83, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panelDatosSesion, GroupLayout.DEFAULT_SIZE, 483, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
+		lblContrasea.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		
+		campoPassword = new JPasswordField();
+		campoPassword.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		
+		JLabel lblUsuario = new JLabel("Usuario:");
+		lblUsuario.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		
+		campoUsuario = new JPasswordField();
+		campoUsuario.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		
+		JButton btnNewButton = new JButton("Aceptar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// TODO: validación datos
+				
+				// TODO: solución temporal
+				/* Habría que usar un card layout como en la pantalla principal porque exige estar cambiando constantemente de capa para poder editarlas */
+				if (campoUsuario.getPassword().length == 0 && campoPassword.getPassword().length == 0) {
+					// TODO: esto es temporal; hay que crear los mensajes en otra clase para poder cambiar de idioma fácilmente
+					JOptionPane.showMessageDialog(contentPane, "Los campos no pueden estar vacíos.", "Error", JOptionPane.ERROR_MESSAGE);
+				}
+				else
+					pantallaLogin.setVisible(false);
+			}
+		});
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		
+		JSeparator separator = new JSeparator();
+		GroupLayout gl_panelDatosSesion = new GroupLayout(panelDatosSesion);
+		gl_panelDatosSesion.setHorizontalGroup(
+			gl_panelDatosSesion.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panelDatosSesion.createSequentialGroup()
+					.addGap(298)
+					.addGroup(gl_panelDatosSesion.createParallelGroup(Alignment.LEADING, false)
+						.addGroup(gl_panelDatosSesion.createSequentialGroup()
+							.addComponent(lblUsuario, GroupLayout.PREFERRED_SIZE, 128, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(campoUsuario, GroupLayout.PREFERRED_SIZE, 188, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panelDatosSesion.createSequentialGroup()
+							.addComponent(lblContrasea)
+							.addGap(18)
+							.addComponent(campoPassword, GroupLayout.PREFERRED_SIZE, 188, GroupLayout.PREFERRED_SIZE))
+						.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addContainerGap(299, Short.MAX_VALUE))
+				.addComponent(separator, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 931, Short.MAX_VALUE)
+		);
+		gl_panelDatosSesion.setVerticalGroup(
+			gl_panelDatosSesion.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panelDatosSesion.createSequentialGroup()
+					.addComponent(separator, GroupLayout.PREFERRED_SIZE, 16, GroupLayout.PREFERRED_SIZE)
+					.addGap(135)
+					.addGroup(gl_panelDatosSesion.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblUsuario, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
+						.addComponent(campoUsuario, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE))
+					.addGap(19)
+					.addGroup(gl_panelDatosSesion.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblContrasea)
+						.addComponent(campoPassword, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addComponent(btnNewButton)
+					.addContainerGap(194, Short.MAX_VALUE))
+		);
+		panelDatosSesion.setLayout(gl_panelDatosSesion);
+		pantallaLogin.setLayout(gl_pantallaLogin);
 		
 		JPanel pantalla = new JPanel();
 		layeredPane.setLayer(pantalla, 1);
