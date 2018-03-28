76f3103bd34504081d9cc045bf15a984c68b2f10
Sergio Cavero <scaverod@gmail.com>
2018-03-29 00:46:21 +0200

    Interfaz servicios

--- SimuladorHotel/src/vista/PanelServicios.java
+++ SimuladorHotel/src/vista/PanelServicios.java
@@ -1,12 +1,17 @@
 package vista;
 
 import java.awt.Dimension;
+import java.awt.Font;
 
 import javax.swing.GroupLayout;
 import javax.swing.JPanel;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PanelServicios extends JPanel {
 
@@ -22,27 +27,80 @@ public class PanelServicios extends JPanel {
 		this.setSize(new Dimension(931, 483));
 		this.setName("panelServicios");
 		
-		JButton btnNewButton = new JButton("New button");
-		
 		JButton btnNewButton_1 = new JButton("New button");
 		
-		JButton btnNewButton_2 = new JButton("New button");
+		JButton btnCambioToalla = new JButton("Cambiar Toallas");
+		btnCambioToalla.setIcon(new ImageIcon(PanelServicios.class.getResource("/iconos/towel.png")));
+		btnCambioToalla.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnCambioToalla.setVerticalTextPosition( SwingConstants.BOTTOM );
+		btnCambioToalla.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnCambioToalla.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
 		
-		JButton btnNewButton_3 = new JButton("New button");
+		JButton btnTelefono = new JButton("Realizar Llamada");
+		btnTelefono.setIcon(new ImageIcon(PanelServicios.class.getResource("/iconos/phone.png")));
+		btnTelefono.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnTelefono.setVerticalTextPosition( SwingConstants.BOTTOM );
+		btnTelefono.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		
+		JButton button_2 = new JButton("New button");
+		
+		JButton button_3 = new JButton("New button");
+		
+		JButton button_4 = new JButton("New button");
+		
+		JButton button_5 = new JButton("New button");
+		
+		JButton button_6 = new JButton("New button");
+		
+		JButton button_7 = new JButton("New button");
+		
+		JButton button_8 = new JButton("New button");
+		
+		JButton button_9 = new JButton("New button");
+		
+		JButton button_10 = new JButton("New button");
+		
+		JButton button_11 = new JButton("New button");
+		
+		JButton button_12 = new JButton("New button");
+		
+		JButton button_13 = new JButton("New button");
 		GroupLayout gl_panelServicios = new GroupLayout(this);
 		gl_panelServicios.setHorizontalGroup(
 			gl_panelServicios.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panelServicios.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(gl_panelServicios.createParallelGroup(Alignment.TRAILING)
-						.addGroup(Alignment.LEADING, gl_panelServicios.createSequentialGroup()
-							.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 272, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED, 458, Short.MAX_VALUE)
-							.addComponent(btnNewButton_2, GroupLayout.PREFERRED_SIZE, 181, GroupLayout.PREFERRED_SIZE))
+					.addGroup(gl_panelServicios.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnCambioToalla, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnTelefono, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE))
+					.addGap(43)
+					.addGroup(gl_panelServicios.createParallelGroup(Alignment.LEADING)
+						.addComponent(button_2, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
+						.addComponent(button_3, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
+						.addComponent(button_4, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE))
+					.addGap(49)
+					.addGroup(gl_panelServicios.createParallelGroup(Alignment.LEADING)
+						.addComponent(button_5, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
+						.addComponent(button_6, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
+						.addComponent(button_7, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE))
+					.addGap(53)
+					.addGroup(gl_panelServicios.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_panelServicios.createSequentialGroup()
-							.addComponent(btnNewButton_3, GroupLayout.PREFERRED_SIZE, 193, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED, 439, Short.MAX_VALUE)
-							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 279, GroupLayout.PREFERRED_SIZE)))
+							.addComponent(button_10, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
+							.addComponent(button_13, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panelServicios.createSequentialGroup()
+							.addComponent(button_9, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
+							.addComponent(button_12, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panelServicios.createSequentialGroup()
+							.addComponent(button_8, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
+							.addComponent(button_11, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)))
 					.addContainerGap())
 		);
 		gl_panelServicios.setVerticalGroup(
@@ -50,12 +108,33 @@ public class PanelServicios extends JPanel {
 				.addGroup(gl_panelServicios.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_panelServicios.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 163, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnNewButton_2, GroupLayout.PREFERRED_SIZE, 144, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED, 187, Short.MAX_VALUE)
-					.addGroup(gl_panelServicios.createParallelGroup(Alignment.TRAILING)
-						.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 111, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnNewButton_3, GroupLayout.PREFERRED_SIZE, 96, GroupLayout.PREFERRED_SIZE))
+						.addComponent(btnCambioToalla, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
+						.addComponent(button_2, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
+						.addComponent(button_5, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
+						.addComponent(button_8, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
+						.addComponent(button_11, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE))
+					.addGap(38)
+					.addGroup(gl_panelServicios.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panelServicios.createSequentialGroup()
+							.addComponent(button_12, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
+							.addComponent(button_13, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panelServicios.createSequentialGroup()
+							.addComponent(button_9, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
+							.addComponent(button_10, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panelServicios.createSequentialGroup()
+							.addComponent(button_6, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
+							.addComponent(button_7, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panelServicios.createSequentialGroup()
+							.addComponent(btnTelefono, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
+							.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panelServicios.createSequentialGroup()
+							.addComponent(button_3, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
+							.addComponent(button_4, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)))
 					.addContainerGap())
 		);
 		this.setLayout(gl_panelServicios);
