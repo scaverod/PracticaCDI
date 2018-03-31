b7b55c70c4986415ed5d908658825d5b2f0801e7
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2018-03-31 19:41:48 +0200

    Editado el panel de servicios (layout final)

--- SimuladorHotel/src/vista/PanelServicios.java
+++ SimuladorHotel/src/vista/PanelServicios.java
@@ -26,128 +26,96 @@ public class PanelServicios extends JPanel {
 		this.setName("panelServicios");
 
 		JButton btnMiniBar = new JButton("Reponer Minibar");
+		btnMiniBar.setBounds(701, 11, 220, 146);
 		btnMiniBar.setIcon(new ImageIcon(PanelServicios.class.getResource("/iconos/minibarnew.png")));
 		btnMiniBar.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnMiniBar.setVerticalTextPosition(SwingConstants.BOTTOM);
 		btnMiniBar.setFont(new Font("Tahoma", Font.PLAIN, 12));
 
 		JButton btnCambioToalla = new JButton("Cambiar Toallas");
+		btnCambioToalla.setBounds(10, 11, 220, 146);
 		btnCambioToalla.setIcon(new ImageIcon(PanelServicios.class.getResource("/iconos/towel.png")));
 		btnCambioToalla.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnCambioToalla.setVerticalTextPosition(SwingConstants.BOTTOM);
 		btnCambioToalla.setFont(new Font("Tahoma", Font.PLAIN, 12));
 
 		JButton btnTelefono = new JButton("Realizar Llamada");
+		btnTelefono.setBounds(10, 168, 220, 146);
 		btnTelefono.setIcon(new ImageIcon(PanelServicios.class.getResource("/iconos/phone.png")));
 		btnTelefono.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnTelefono.setVerticalTextPosition(SwingConstants.BOTTOM);
 		btnTelefono.setFont(new Font("Tahoma", Font.PLAIN, 12));
 
 		JButton btnHorarioLimpieza = new JButton("Hora Limpieza");
+		btnHorarioLimpieza.setBounds(701, 168, 220, 146);
 		btnHorarioLimpieza.setIcon(new ImageIcon(PanelServicios.class.getResource("/iconos/horariolimpieza.png")));
 		btnHorarioLimpieza.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnHorarioLimpieza.setVerticalTextPosition(SwingConstants.BOTTOM);
 		btnHorarioLimpieza.setFont(new Font("Tahoma", Font.PLAIN, 12));
 
 		JButton btnBotones = new JButton("Llamar al Botones");
+		btnBotones.setBounds(240, 168, 220, 146);
 		btnBotones.setIcon(new ImageIcon(PanelServicios.class.getResource("/iconos/botones.png")));
 		btnBotones.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnBotones.setVerticalTextPosition(SwingConstants.BOTTOM);
 		btnBotones.setFont(new Font("Tahoma", Font.PLAIN, 12));
 
 		JButton btnCambioAlmohada = new JButton("Cambiar Almohada");
+		btnCambioAlmohada.setBounds(471, 11, 220, 146);
 		btnCambioAlmohada.setIcon(new ImageIcon(PanelServicios.class.getResource("/iconos/almohada.png")));
 		btnCambioAlmohada.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnCambioAlmohada.setVerticalTextPosition(SwingConstants.BOTTOM);
 		btnCambioAlmohada.setFont(new Font("Tahoma", Font.PLAIN, 12));
 
 		JButton btnCambiarSabanas = new JButton("Cambiar Sabanas");
+		btnCambiarSabanas.setBounds(240, 11, 220, 146);
 		btnCambiarSabanas.setIcon(new ImageIcon(PanelServicios.class.getResource("/iconos/cambioropacama.png")));
 		btnCambiarSabanas.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnCambiarSabanas.setVerticalTextPosition(SwingConstants.BOTTOM);
 		btnCambiarSabanas.setFont(new Font("Tahoma", Font.PLAIN, 12));
 
 		JButton btnComidaHab = new JButton("Solicitar Comida");
+		btnComidaHab.setBounds(471, 168, 220, 146);
 		btnComidaHab.setIcon(new ImageIcon(PanelServicios.class.getResource("/iconos/comidahab.png")));
 		btnComidaHab.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnComidaHab.setVerticalTextPosition(SwingConstants.BOTTOM);
 		btnComidaHab.setFont(new Font("Tahoma", Font.PLAIN, 12));
 
 		JButton btnPedirTaxi = new JButton("Pedir Taxi");
+		btnPedirTaxi.setBounds(240, 325, 220, 146);
 		btnPedirTaxi.setIcon(new ImageIcon(PanelServicios.class.getResource("/iconos/taxi.png")));
 		btnPedirTaxi.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnPedirTaxi.setVerticalTextPosition(SwingConstants.BOTTOM);
 		btnPedirTaxi.setFont(new Font("Tahoma", Font.PLAIN, 12));
 
 		JButton btnMusica = new JButton("New button");
+		btnMusica.setBounds(471, 325, 220, 146);
 
 		JButton btnWifi = new JButton("Activar Wifi");
+		btnWifi.setBounds(10, 325, 220, 146);
 		btnWifi.setIcon(new ImageIcon(PanelServicios.class.getResource("/iconos/wifi.png")));
 		btnWifi.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnWifi.setVerticalTextPosition(SwingConstants.BOTTOM);
 		btnWifi.setFont(new Font("Tahoma", Font.PLAIN, 12));
 
 		JButton btnInformacion = new JButton("Solicitar Informaci\u00F3n");
+		btnInformacion.setBounds(701, 325, 220, 146);
 		btnInformacion.setIcon(new ImageIcon(PanelServicios.class.getResource("/iconos/info.png")));
 		btnInformacion.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnInformacion.setVerticalTextPosition(SwingConstants.BOTTOM);
 		btnPedirTaxi.setFont(new Font("Tahoma", Font.PLAIN, 12));
-
-		GroupLayout gl_panelServicios = new GroupLayout(this);
-		gl_panelServicios.setHorizontalGroup(
-			gl_panelServicios.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panelServicios.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_panelServicios.createParallelGroup(Alignment.TRAILING)
-						.addComponent(btnCambioToalla, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnTelefono, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnBotones, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_panelServicios.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panelServicios.createSequentialGroup()
-							.addGroup(gl_panelServicios.createParallelGroup(Alignment.LEADING)
-								.addComponent(btnCambioAlmohada, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
-								.addComponent(btnWifi, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE))
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(gl_panelServicios.createParallelGroup(Alignment.LEADING)
-								.addComponent(btnHorarioLimpieza, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
-								.addComponent(btnMiniBar, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)))
-						.addGroup(gl_panelServicios.createSequentialGroup()
-							.addComponent(btnComidaHab, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(btnMusica, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_panelServicios.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnCambiarSabanas, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnPedirTaxi, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnInformacion, GroupLayout.PREFERRED_SIZE, 143, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(622, Short.MAX_VALUE))
-		);
-		gl_panelServicios.setVerticalGroup(
-			gl_panelServicios.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panelServicios.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_panelServicios.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnCambioToalla, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnCambioAlmohada, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnMiniBar, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnCambiarSabanas, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_panelServicios.createParallelGroup(Alignment.TRAILING)
-						.addComponent(btnTelefono, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnWifi, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
-						.addGroup(Alignment.LEADING, gl_panelServicios.createParallelGroup(Alignment.BASELINE)
-							.addComponent(btnHorarioLimpieza, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
-							.addComponent(btnPedirTaxi, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_panelServicios.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panelServicios.createParallelGroup(Alignment.BASELINE)
-							.addComponent(btnComidaHab, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
-							.addComponent(btnBotones, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE))
-						.addComponent(btnMusica, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnInformacion, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(323, Short.MAX_VALUE))
-		);
-		this.setLayout(gl_panelServicios);
+		setLayout(null);
+		add(btnComidaHab);
+		add(btnMusica);
+		add(btnTelefono);
+		add(btnBotones);
+		add(btnHorarioLimpieza);
+		add(btnPedirTaxi);
+		add(btnInformacion);
+		add(btnWifi);
+		add(btnCambioToalla);
+		add(btnCambioAlmohada);
+		add(btnCambiarSabanas);
+		add(btnMiniBar);
 	}
 }
