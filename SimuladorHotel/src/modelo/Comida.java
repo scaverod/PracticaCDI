c394940289be4c40a473ff8726d40973de482d5e
Sergio Cavero <scaverod@gmail.com>
2018-04-05 11:59:15 +0200

    Añadido comida

---
+++ SimuladorHotel/src/modelo/Comida.java
@@ -0,0 +1,19 @@
+package modelo;
+
+public class Comida {
+
+	private double precio;
+
+	public Comida(double precio) {
+		this.precio = precio;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+}
--- SimuladorHotel/src/modelo/Servicios.java
+++ SimuladorHotel/src/modelo/Servicios.java
@@ -10,6 +10,7 @@ public class Servicios {
 	private Llamada llamada;
 	private Sabanas sabanas;
 	private Limpieza limpieza;
+	private Comida comida;
 
 	public Servicios() {
 		this.almohada = new Almohada(1.50, 2, 1.25, 1);
@@ -20,6 +21,23 @@ public class Servicios {
 		this.llamada = new Llamada(0.50, 1.25, 1.50, 2);
 		this.sabanas = new Sabanas(0.50);
 		this.limpieza = new Limpieza(0.50, null);
+		this.comida = new Comida(3.50);
+	}
+
+	public Comida getComida() {
+		return comida;
+	}
+
+	public void setComida(Comida comida) {
+		this.comida = comida;
+	}
+
+	public Limpieza getLimpieza() {
+		return limpieza;
+	}
+
+	public void setLimpieza(Limpieza limpieza) {
+		this.limpieza = limpieza;
 	}
 
 	public Sabanas getSabanas() {
