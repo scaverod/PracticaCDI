0fbbe0294600a8439cd7e481fd6caa601dbacbb7
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2018-04-12 13:38:37 +0200

    Terminada implementacion PanelSpa

--- SimuladorHotel/src/vista/PanelSpa.java
+++ SimuladorHotel/src/vista/PanelSpa.java
@@ -12,10 +12,10 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLayeredPane;
 import java.awt.CardLayout;
-import java.awt.event.HierarchyListener;
-import java.awt.event.HierarchyEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 public class PanelSpa extends JPanel {
 	private static final long serialVersionUID = 1L;
@@ -24,13 +24,16 @@ public class PanelSpa extends JPanel {
 	private JPanel panelPrincipal;
 	private JPanel panelBase;
 	private JButton btnOtroBoton;
+	private JLabel lblNewLabel;
+	private JLayeredPane layeredPane;
+	private JPanel panelOtro;
 
 	public PanelSpa (Controlador controlador) {
 		this.setSize(new Dimension(931, 483));
 		this.setName("p" + this.getClass().getSimpleName().substring(1));
 		setLayout(null);
 		
-		JLayeredPane layeredPane = new JLayeredPane();
+		layeredPane = new JLayeredPane();
 		layeredPane.setBounds(0, 0, 931, 483);
 		add(layeredPane);
 		
@@ -40,28 +43,45 @@ public class PanelSpa extends JPanel {
 		layeredPane.add(panelPrincipal);
 		panelPrincipal.setLayout(null);
 		
-		JButton btnConEstoSaco = new JButton("Con esto saco un panel emergente");
+		JButton btnConEstoSaco = new JButton("Con esto saco UN DETERMINADO panel emergente");
 		btnConEstoSaco.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				CardLayout l = (CardLayout) panelEmergenteContenedor.getLayout();
-				l.show(panelEmergenteContenedor, panelBase.getName());
-				
-				layeredPane.setLayer(panelEmergenteContenedor, 2);
-				btnOtroBoton.setEnabled(false);
+				changeToPanelEmergente(panelBase);
 			}
 		});
 		btnConEstoSaco.setBounds(290, 160, 351, 162);
 		panelPrincipal.add(btnConEstoSaco);
 		
 		btnOtroBoton = new JButton("Otro boton");
+		btnOtroBoton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				changeToPanelEmergente(panelOtro);
+			}
+		});
+		btnOtroBoton.setIcon(new ImageIcon(PanelSpa.class.getResource("/iconos/tick.png")));
 		btnOtroBoton.setBounds(31, 36, 211, 109);
 		panelPrincipal.add(btnOtroBoton);
+		
+		lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon(PanelSpa.class.getResource("/iconos/espa\u00F1a.png")));
+		lblNewLabel.setBounds(54, 335, 311, 109);
+		panelPrincipal.add(lblNewLabel);
 		
 		panelEmergenteContenedor = new JPanel();
 		panelEmergenteContenedor.addPropertyChangeListener(new PropertyChangeListener() {
 			public void propertyChange(PropertyChangeEvent evt) {
-				
-				System.out.println(evt.getPropertyName());
+				if (evt.getNewValue().equals(0)) {
+					//TODO: reactivar elementos del panelPrincipal (ahora panel al frente)
+					btnConEstoSaco.setEnabled(true);
+					btnOtroBoton.setEnabled(true);
+					lblNewLabel.setEnabled(true);
+				}
+				else if (evt.getNewValue().equals(2)) {
+					// TODO: desactivar elementos del panelPrincipal (ahora panel al fondo)
+					btnConEstoSaco.setEnabled(false);
+					btnOtroBoton.setEnabled(false);
+					lblNewLabel.setEnabled(false);
+				}
 			}
 		});
 		layeredPane.setLayer(panelEmergenteContenedor, 0);
@@ -70,6 +90,18 @@ public class PanelSpa extends JPanel {
 		panelEmergenteContenedor.setLayout(new CardLayout(0, 0));
 		
 		panelBase = new PanelSpaEmergenteBase(new MicroControladorLayersPadreHijo(layeredPane, panelEmergenteContenedor));
-		panelEmergenteContenedor.add(panelBase, "panelBase");
+		panelEmergenteContenedor.add(panelBase, panelBase.getName());
+		
+		panelOtro = new PanelSpaEmergenteOtro(new MicroControladorLayersPadreHijo(layeredPane, panelEmergenteContenedor));
+		panelEmergenteContenedor.add(panelOtro, panelOtro.getName());
+	}
+	
+	private void changeToPanelEmergente(JPanel subPanel) {
+		// Cambio el panel activo dentro del panel emergente
+		CardLayout l = (CardLayout) panelEmergenteContenedor.getLayout();
+		l.show(panelEmergenteContenedor, subPanel.getName());
+		
+		// Pongo el panel emergente por encima del principal
+		layeredPane.setLayer(panelEmergenteContenedor, 2);
 	}
 }
