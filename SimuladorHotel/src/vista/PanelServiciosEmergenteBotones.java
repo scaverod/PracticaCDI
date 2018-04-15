0963cc04687e6d8eaa73495753a48b5df58c8dc6
Sergio Cavero <scaverod@gmail.com>
2018-04-15 15:49:38 +0200

    diseño de la interfaz y back end

--- SimuladorHotel/src/vista/PanelServiciosEmergenteBotones.java
+++ SimuladorHotel/src/vista/PanelServiciosEmergenteBotones.java
@@ -3,22 +3,27 @@ package vista;
 import java.awt.Dimension;
 
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
 import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
 
 import controlador.Controlador;
 import controlador.MicroControladorLayers;
 import controlador.MicroControladorPanelesPadreHijo;
 import idiomas.Texto;
 import idiomas.TextoManager;
+import tiposVariable.StringDouble;
 
 import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.Font;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.util.concurrent.Semaphore;
 import java.awt.event.ActionEvent;
-import javax.swing.SwingConstants;
+
 import javax.swing.JLayeredPane;
 
 public class PanelServiciosEmergenteBotones extends JPanel {
@@ -55,12 +60,6 @@ public class PanelServiciosEmergenteBotones extends JPanel {
 
 		crearPanelConfirmacion("<precio>");
 
-		JLabel lblPanelEmergente = new JLabel(this.getName());
-		lblPanelEmergente.setHorizontalAlignment(SwingConstants.CENTER);
-		lblPanelEmergente.setFont(new Font("Tahoma", Font.PLAIN, 32));
-		lblPanelEmergente.setBounds(10, 138, 675, 39);
-		panelPrincipal.add(lblPanelEmergente);
-
 		JButton btnCerrar = new JButton(t.getBtnCerrar());
 		btnCerrar.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnCerrar.addActionListener(new ActionListener() {
@@ -73,18 +72,56 @@ public class PanelServiciosEmergenteBotones extends JPanel {
 		btnCerrar.setBounds(610, 11, 75, 50);
 		panelPrincipal.add(btnCerrar);
 
-		JButton btnNewButton = new JButton("botonEjemploVentanaEmergente");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		JLabel lblGif = new JLabel("");
+		lblGif.setBorder(new LineBorder(Color.GREEN));
+		lblGif.setOpaque(true);
+		lblGif.setVisible(false);
+		lblGif.setIcon(new ImageIcon(PanelServiciosEmergenteWifi.class.getResource("/iconos/check-gif-1.gif")));
+		lblGif.setBounds(287, 103, 120, 109);
+		panelPrincipal.add(lblGif);
+
+		JTextPane txtpnInfo = new JTextPane();
+		txtpnInfo.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtpnInfo.setEditable(false);
+		txtpnInfo.setOpaque(false);
+		txtpnInfo.setText(t.getTxtInformacion());
+		txtpnInfo.setBounds(45, 63, 593, 75);
+		panelPrincipal.add(txtpnInfo);
+
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 153));
+		panel.setBorder(new LineBorder(Color.ORANGE, 3));
+		panel.setForeground(Color.ORANGE);
+		panel.setBounds(45, 166, 593, 35);
+		panelPrincipal.add(panel);
+
+		JLabel lblPrecio = new JLabel(t.getLblCoste());
+		panel.add(lblPrecio);
+		lblPrecio.setFont(new Font("Tahoma", Font.PLAIN, 15));
+
+		JLabel label = new JLabel(String.valueOf(controlador.getServicios().getBotones().getPrecio()) + " €");
+		panel.add(label);
+		label.setFont(new Font("Tahoma", Font.PLAIN, 15));
+
+		JButton btnAdquirir = new JButton(t.getBtnAdquirir());
+		btnAdquirir.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnAdquirir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
 				new Thread() {
 					public void run() {
 						try {
-							mostrarPanelConfirmacion("<precio>");
+
+							mostrarPanelConfirmacion(
+									String.valueOf(controlador.getServicios().getBotones().getPrecio()) + " €");
 
 							s.acquire();
 
 							if (((PanelConfirmacionServicios) panelConfirmacion).getConfirmacion() == true) {
-								// Actualizar ventana; en otro caso no hacer nada
+								lblGif.setVisible(true);
+								Thread.sleep(2050);
+								lblGif.setVisible(false);
+								controlador.getCuenta().getGasto().addGasto(new StringDouble("Llamada al botones",
+										controlador.getServicios().getBotones().getPrecio()));
 
 								// Tiene que hacerse siempre!
 								((PanelConfirmacionServicios) panelConfirmacion).setConfirmacion(false);
@@ -96,8 +133,8 @@ public class PanelServiciosEmergenteBotones extends JPanel {
 				}.start();
 			}
 		});
-		btnNewButton.setBounds(253, 230, 189, 23);
-		panelPrincipal.add(btnNewButton);
+		btnAdquirir.setBounds(260, 223, 175, 53);
+		panelPrincipal.add(btnAdquirir);
 	}
 
 	public void cerrarPanelConfirmacion() {
