94bb9c7ad5566f96e7e38bd62d8f2df3f525b5b0
Sergio Cavero <scaverod@gmail.com>
2018-04-17 20:25:18 +0200

    plantilla creada

--- SimuladorHotel/src/vista/PanelServiciosEmergenteSabanas.java
+++ SimuladorHotel/src/vista/PanelServiciosEmergenteSabanas.java
@@ -3,7 +3,9 @@ package vista;
 import java.awt.Dimension;
 
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
 import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
 
 import controlador.Controlador;
 import controlador.MicroControladorLayers;
@@ -14,11 +16,12 @@ import idiomas.TextoManager;
 import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.Font;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.util.concurrent.Semaphore;
 import java.awt.event.ActionEvent;
-import javax.swing.SwingConstants;
 import javax.swing.JLayeredPane;
 
 public class PanelServiciosEmergenteSabanas extends JPanel {
@@ -55,12 +58,6 @@ public class PanelServiciosEmergenteSabanas extends JPanel {
 
 		crearPanelConfirmacion("<precio>");
 
-		JLabel lblPanelEmergente = new JLabel(this.getName());
-		lblPanelEmergente.setHorizontalAlignment(SwingConstants.CENTER);
-		lblPanelEmergente.setFont(new Font("Tahoma", Font.PLAIN, 32));
-		lblPanelEmergente.setBounds(10, 138, 675, 39);
-		panelPrincipal.add(lblPanelEmergente);
-
 		JButton btnCerrar = new JButton(t.getBtnCerrar());
 		btnCerrar.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnCerrar.addActionListener(new ActionListener() {
@@ -73,8 +70,17 @@ public class PanelServiciosEmergenteSabanas extends JPanel {
 		btnCerrar.setBounds(610, 11, 75, 50);
 		panelPrincipal.add(btnCerrar);
 
-		JButton btnNewButton = new JButton("botonEjemploVentanaEmergente");
-		btnNewButton.addActionListener(new ActionListener() {
+		JLabel lblGif = new JLabel("");
+		lblGif.setBorder(new LineBorder(Color.GREEN));
+		lblGif.setOpaque(true);
+		lblGif.setVisible(false);
+		lblGif.setIcon(new ImageIcon(PanelServiciosEmergenteWifi.class.getResource("/iconos/check-gif-1.gif")));
+		lblGif.setBounds(287, 103, 120, 109);
+		panelPrincipal.add(lblGif);
+
+		JButton btnAdquirir = new JButton(t.getBtnAdquirir());
+		btnAdquirir.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnAdquirir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				new Thread() {
 					public void run() {
@@ -84,6 +90,9 @@ public class PanelServiciosEmergenteSabanas extends JPanel {
 							s.acquire();
 
 							if (((PanelConfirmacionServicios) panelConfirmacion).getConfirmacion() == true) {
+								lblGif.setVisible(true);
+								Thread.sleep(2050);
+								lblGif.setVisible(false);
 								// Actualizar ventana; en otro caso no hacer nada
 
 								// Tiene que hacerse siempre!
@@ -96,8 +105,31 @@ public class PanelServiciosEmergenteSabanas extends JPanel {
 				}.start();
 			}
 		});
-		btnNewButton.setBounds(253, 230, 189, 23);
-		panelPrincipal.add(btnNewButton);
+		btnAdquirir.setBounds(260, 223, 175, 53);
+		panelPrincipal.add(btnAdquirir);
+
+		JTextPane txtpnInfo = new JTextPane();
+		txtpnInfo.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtpnInfo.setEditable(false);
+		txtpnInfo.setOpaque(false);
+		txtpnInfo.setText(t.getTxtInformacion());
+		txtpnInfo.setBounds(45, 63, 593, 75);
+		panelPrincipal.add(txtpnInfo);
+
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 153));
+		panel.setBorder(new LineBorder(Color.ORANGE, 3));
+		panel.setForeground(Color.ORANGE);
+		panel.setBounds(45, 166, 593, 35);
+		panelPrincipal.add(panel);
+
+		JLabel lblPrecio = new JLabel(t.getLblCoste());
+		panel.add(lblPrecio);
+		lblPrecio.setFont(new Font("Tahoma", Font.PLAIN, 15));
+
+		JLabel label = new JLabel("0\u20AC");
+		panel.add(label);
+		label.setFont(new Font("Tahoma", Font.PLAIN, 15));
 	}
 
 	public void cerrarPanelConfirmacion() {
