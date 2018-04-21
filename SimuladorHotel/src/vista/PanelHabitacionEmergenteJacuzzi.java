51adf2a2b8a541e88f9663081212d493c28601ce
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2018-04-21 15:05:00 +0200

    Progreso en panelJacuzzi

--- SimuladorHotel/src/vista/PanelHabitacionEmergenteJacuzzi.java
+++ SimuladorHotel/src/vista/PanelHabitacionEmergenteJacuzzi.java
@@ -13,7 +13,12 @@ import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
+import javax.swing.JSlider;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.JToggleButton;
 
 public class PanelHabitacionEmergenteJacuzzi extends JPanel {
 	private static final long serialVersionUID = 1L;
@@ -27,12 +32,6 @@ public class PanelHabitacionEmergenteJacuzzi extends JPanel {
 		this.setName("p" + this.getClass().getSimpleName().substring(1));
 		setLayout(null);
 
-		JLabel lblPanelEmergente = new JLabel(this.getName());
-		lblPanelEmergente.setHorizontalAlignment(SwingConstants.CENTER);
-		lblPanelEmergente.setFont(new Font("Tahoma", Font.PLAIN, 32));
-		lblPanelEmergente.setBounds(10, 138, 675, 39);
-		add(lblPanelEmergente);
-
 		JButton btnCerrar = new JButton(t.getBtnCerrar());
 		btnCerrar.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnCerrar.addActionListener(new ActionListener() {
@@ -44,16 +43,91 @@ public class PanelHabitacionEmergenteJacuzzi extends JPanel {
 		btnCerrar.setBounds(610, 11, 75, 50);
 		add(btnCerrar);
 		
-		JLabel lblEncenderapagar = new JLabel("encender/apagar burbujas/chorros");
-		lblEncenderapagar.setBounds(10, 259, 173, 45);
-		add(lblEncenderapagar);
+		JLabel lblBurbujas = new JLabel("");
+		lblBurbujas.setBounds(485, 104, 200, 200);
+		add(lblBurbujas);
 		
-		JLabel lblRellenar = new JLabel("rellenar");
-		lblRellenar.setBounds(214, 259, 150, 45);
-		add(lblRellenar);
+		JLabel lbljacuzzi = new JLabel("");
+		lbljacuzzi.setBounds(485, 104, 200, 200);
+		add(lbljacuzzi);
+		lbljacuzzi.setIcon(new ImageIcon(PanelHabitacionEmergenteJacuzzi.class.getResource("/iconos/jacuzziVacio.png")));
 		
-		JLabel lblTemperatura = new JLabel("temperatura");
-		lblTemperatura.setBounds(395, 259, 143, 45);
-		add(lblTemperatura);
+		JToggleButton tglBtnBurbujas = new JToggleButton("Activar/Desactivar");
+		tglBtnBurbujas.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		tglBtnBurbujas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (tglBtnBurbujas.isSelected())
+					lblBurbujas.setIcon(new ImageIcon(PanelHabitacionEmergenteJacuzzi.class.getResource("/iconos/jacuzziBurbujas.png")));
+				else
+					lblBurbujas.setIcon(null);
+			}
+		});
+		tglBtnBurbujas.setBounds(307, 106, 151, 27);
+		add(tglBtnBurbujas);
+		
+		JPanel panelTemperatura = new JPanel();
+		panelTemperatura.setBorder(new TitledBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 109, 240), new Color(0, 109, 240),
+						new Color(0, 109, 240), new Color(0, 109, 240)), "Temperatura", TitledBorder.LEADING, TitledBorder.TOP, new Font("Tahoma", Font.PLAIN, 15), null));
+		panelTemperatura.setBounds(10, 239, 465, 65);
+		add(panelTemperatura);
+
+		JPanel panelAgua = new JPanel();
+		panelAgua.setBorder(new TitledBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 109, 240), new Color(0, 109, 240),
+				new Color(0, 109, 240), new Color(0, 109, 240)), "Agua", TitledBorder.LEADING, TitledBorder.TOP, new Font("Tahoma", Font.PLAIN, 15), null));
+		panelAgua.setBounds(10, 163, 465, 65);
+		add(panelAgua);
+		panelAgua.setLayout(null);
+		
+		JSlider slider = new JSlider();
+		slider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				if (slider.getValue() == 0) {
+					tglBtnBurbujas.setEnabled(false);
+					lblBurbujas.setIcon(null);
+				}
+				else {
+					tglBtnBurbujas.setEnabled(true);
+					if (tglBtnBurbujas.isSelected())
+						lblBurbujas.setIcon(new ImageIcon(PanelHabitacionEmergenteJacuzzi.class.getResource("/iconos/jacuzziBurbujas.png")));
+				}
+				
+				switch (slider.getValue()) {
+					case 0:
+						lbljacuzzi.setIcon(new ImageIcon(PanelHabitacionEmergenteJacuzzi.class.getResource("/iconos/jacuzziVacio.png")));
+						break;
+					case 25:
+						lbljacuzzi.setIcon(new ImageIcon(PanelHabitacionEmergenteJacuzzi.class.getResource("/iconos/jacuzzi25.png")));
+						break;
+					case 50:
+						lbljacuzzi.setIcon(new ImageIcon(PanelHabitacionEmergenteJacuzzi.class.getResource("/iconos/jacuzzi50.png")));
+						break;
+					case 75:
+						lbljacuzzi.setIcon(new ImageIcon(PanelHabitacionEmergenteJacuzzi.class.getResource("/iconos/jacuzzi75.png")));
+						break;
+					case 100:
+						lbljacuzzi.setIcon(new ImageIcon(PanelHabitacionEmergenteJacuzzi.class.getResource("/iconos/jacuzzi100.png")));
+						break;
+				}
+			}
+		});
+		slider.setBounds(206, 11, 200, 50);
+		panelAgua.add(slider);
+		slider.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		slider.setPaintLabels(true);
+		slider.setPaintTicks(true);
+		slider.setSnapToTicks(true);
+		slider.setValue(0);
+		slider.setMajorTickSpacing(25);
+		slider.setMinorTickSpacing(25);
+		
+		JLabel lblNewLabel = new JLabel("Rellenar(%):");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel.setBounds(62, 26, 82, 20);
+		panelAgua.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(10, 11, 46, 14);
+		add(lblNewLabel_1);
 	}
 }
